7e3488d93f5e6b378a564487bda9e44a46809b3c
Paul Bui <paulbui@alumni.virginia.edu>
2020-09-19 12:59:43 -0400

    Added master branch identifier to CDN URL path

--- gradebox_tweaks/installer/gradebox_tweaks_installer.user.js
+++ gradebox_tweaks/installer/gradebox_tweaks_installer.user.js
@@ -18,7 +18,7 @@
   
     if (typeof GradeBoxTweaks !== 'function') {
       const script = document.createElement('script');
-      script.src = 'https://cdn.jsdelivr.net/gh/paulbui/canvas-tweaks/gradebox_tweaks/src/gradebox_tweaks.js';
+      script.src = 'https://cdn.jsdelivr.net/gh/paulbui/canvas-tweaks@master/gradebox_tweaks/src/gradebox_tweaks.js';
       script.onload = function() {
         GradeBoxTweaks(config);
       };
