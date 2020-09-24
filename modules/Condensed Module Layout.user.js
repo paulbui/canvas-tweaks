d88b4aa5568770ece6569b6a40ad40ba157290e3
Paul Bui <paulbui@users.noreply.github.com>
2020-09-24 13:26:42 -0400

    Create Condensed Module Layout.user.js

---
+++ modules/Condensed Module Layout.user.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         Condensed Module Layout
+// @namespace    https://github.com/paulbui/canvas-tweaks
+// @version      0.1
+// @description  Condensed layout of the Canvas Modules page
+// @author       Paul Bui
+// @match        http*://*/courses/*/modules
+// @grant        none
+// @run-at       document-body
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var sheet = document.createElement('style');
+    sheet.innerHTML = ".context_module {margin-top: 0px !important;}";
+    sheet.innerHTML += ".item-group-condensed {padding : 0px !important;}";
+    sheet.innerHTML += ".item-group-condensed .ig-header {padding : 0px 0px !important; margin-top: 1px !important;}";
+    sheet.innerHTML += ".ig-list .ig-row {padding: 1px 0px 1px 0px !important;}";
+    sheet.innerHTML += ".ig-list {font-size : 0.9rem !important;}"
+    sheet.innerHTML += ".locked_title {font-size : 1rem !important;}";
+    document.body.appendChild(sheet);
+
+})();
