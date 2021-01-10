79599a38b1343d9ade263992ff3e586f467008bd
Paul Bui <paulbui@alumni.virginia.edu>
2021-01-09 23:35:22 -0500

    Added missing assignments list folder and script

---
+++ missing_assignments_list/missing_assignments_list.user.js
@@ -0,0 +1,17 @@
+// ==UserScript==
+// @name         Missing Assignments List
+// @namespace    https://github.com/paulbui/canvas-tweaks
+// @version      0.1
+// @description  Adds list of missing assignments to right sidebar
+// @author       Paul Bui
+// @match        http*://*.instructure.com/courses/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+  'use strict';
+
+  
+
+
+})();
